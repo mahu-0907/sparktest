484876f7f8f36336be75d773b2abc1ed469c9c1d
mahu <73730907+mahu-0907@users.noreply.github.com>
2022-04-24 14:38:18 +0800

    Update javatest.java

--- Coretest/src/main/java/javatest/javatest.java
+++ Coretest/src/main/java/javatest/javatest.java
@@ -4,5 +4,6 @@ public class javatest {
     public static void main(String[] args) {
         System.out.println("这是第一次编写的代码！！");
         System.out.println("这是第二次编写的代码，本地提交！！");
+        System.out.println("这是在GitHub上编写的代码，测试远程拉取！！");
     }
 }
